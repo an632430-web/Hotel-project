cca4837794b0e122defb037fd971a621dabe4c93
an632430-web <an632430@gmail.com>
2026-09-15 03:15:00 +0300

    first commit

---
+++ client/src/components/StarRating.jsx
@@ -0,0 +1,13 @@
+import { assets } from "../assets/assets"
+
+const StarRating = ({rating = 4}) => {
+  return (
+    <div className="flex">
+         {Array(5).fill('').map((_, index) => (
+                 <img key={index} src={rating >index ? assets.starIconFilled:assets.starIconOutlined} className="w-4.5 h-4.5"/>
+                            ))}
+    </div>
+  )
+}
+
+export default StarRating
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+
+import Hero from '../components/Hero'
+import FeaturedDestination from '../components/FeaturedDestination'
+import ExlusiveOffers from '../components/ExlusiveOffers'
+import Testimonial from '../components/Testimonial'
+import Newsletter from '../components/Newsletter'
+
+const Home = () => {
+  return (
+    <>
+        <Hero/>
+        <FeaturedDestination/>
+        <ExlusiveOffers/>
+        <Testimonial/>
+        <Newsletter/>
+    </>
+    
+  )
+}
+
+export default Home
